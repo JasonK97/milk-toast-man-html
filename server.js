49269d17abe0b93e4d8083569309b415987c405e
Jason Kent <jasonbkent@gmail.com>
2020-05-06 21:57:10 -0600

    Node implementation

--- server.js
+++ server.js
@@ -2,17 +2,6 @@ var express = require("express");
 var app = express();
 require('dotenv').config();
 
-const campaignController = require("./controllers/campaignController.js");
-
-var session = require("express-session");
-
-// set up sessions
-app.use(session({
-  secret: "my-secret",
-  resave: false,
-  saveUninitialized: true
-}));
-
 app.use(express.static("public"));
 app.use(express.json()); //support json encoded bodies
 app.use(express.urlencoded({extended: true})); //support url encoded bodies
@@ -20,14 +9,7 @@ app.use(express.urlencoded({extended: true})); //support url encoded bodies
 
 app.set("port", (process.env.PORT || 5000));
 
-app.get("/getCampaigns", campaignController.getCampaignList);
-app.get("/getSpecificCampaign", campaignController.getSpecificCampaign);
-app.get("/searchByFree", campaignController.searchByFree);
-app.get("/searchByPaid", campaignController.searchByPaid);
-
-app.post("/login", campaignController.handleLogin);
-app.post("/logout", campaignController.handleLogout);
-app.post("/insertCampaign", campaignController.insertCampaign);
+app.get("/home");
 
 app.listen(app.get("port"), function() {
     console.log("Now listening on port: ", app.get("port"));
