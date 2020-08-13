78f32bc77ed9bcc4b5a9cf7f674989bae8af9f10
Jason Kent <jasonbkent@gmail.com>
2020-08-12 18:34:25 -0600

    Stuff

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ require('dotenv').config();
 app.use(express.static("public"));
 app.use(express.json()); //support json encoded bodies
 app.use(express.urlencoded({extended: true})); //support url encoded bodies
-//app.use(campainController.logRequest);
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 app.set("port", (process.env.PORT || 5000));
 
 app.get("/home");
+app.get('/', function (req, res) {
+    res.render("client");
+  });
 
 app.listen(app.get("port"), function() {
     console.log("Now listening on port: ", app.get("port"));
